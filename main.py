34e4fd0f0edab45d27e96237848b1e9bd89a0704
PedroGomes-phgr <PedroGomes-phgr@users.noreply.github.com>
2026-03-20 22:12:17 +0000

    feat: favicon SVG, navbar unificada e empty state com pills
    - Favicon SVG servido em /favicon.svg (rota FastAPI) com símbolo UnB
      em branco sobre fundo #003F72; referenciado no <head>; elimina 404
    - Hero gradient alterado de 135deg para 180deg (vertical), eliminando
      a linha de cor visível entre navbar e hero
    - Empty state substituído por pills clicáveis de sugestão de busca
      (Cálculo, Programação, Física, Química, Administração, Estatística,
      Direito, Inglês, Economia, Biologia) + contador de turmas
    - Click nas pills preenche o campo de busca e dispara a pesquisa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import psycopg2
 import psycopg2.extras
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, Response
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
@@ -120,6 +120,18 @@ async def update_link(
                 save_link_to_db(materia, turma, link)
             break
     return JSONResponse(content={"ok": True})
+
+
+@app.get("/favicon.svg", include_in_schema=False)
+async def favicon():
+    svg = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="7 -152 634 634">
+  <rect x="7" y="-152" width="634" height="634" fill="#003F72"/>
+  <path fill="white" opacity="0.95" d="m 543.63956,122.52025 c -31.99939,23.06828 -66.98325,43.06013 -104.68075,59.36796 -18.62882,8.05762 -38.70764,13.629 -59.85377,16.27428 -15.03676,1.87867 -30.34934,2.82421 -45.88061,2.82421 l 0,123.26981 308.17389,0 0,-272.32649 -97.75007,70.60017 -0.009,-0.01"/>
+  <path fill="white" opacity="0.95" d="m 105.18666,122.52025 c 32.00957,23.06828 66.98846,43.06013 104.69093,59.36796 18.63255,8.05762 38.70143,13.629 59.8575,16.27428 15.02681,1.87867 30.3394,2.82421 45.88061,2.82421 l 0,123.26981 -308.1838357,0 0,-272.32649 97.7499457,70.60017 0.005,-0.01"/>
+  <path fill="white" opacity="0.65" d="m 333.22443,7.27328 0,176.09972 c 14.78825,0 29.37894,-0.912 43.69132,-2.6838 l 0,0 c 19.4526,-2.43655 37.9112,-7.5569 55.04405,-14.96345 36.51712,-15.79343 70.37528,-35.14415 101.36575,-57.48929 l 0,0.005 108.07277,-78.05021 0,-22.91794 -308.17389,0"/>
+  <path fill="white" opacity="0.65" d="m 315.6157,7.27328 0,176.09972 c -14.80316,0 -29.3777,-0.912 -43.69132,-2.6838 l -0.005,0 c -19.45757,-2.43655 -37.9199,-7.5569 -55.05275,-14.96345 -36.50842,-15.79343 -70.36658,-35.14415 -101.36625,-57.48929 l 0,0.005 -108.0685457,-78.05021 0,-22.91794 308.1838357,0"/>
+</svg>"""
+    return Response(content=svg, media_type="image/svg+xml")
 
 
 @app.get("/json")
